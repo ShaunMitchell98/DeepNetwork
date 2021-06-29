f5f8d42777c8fc23acba42d15553093a332b44ed
Shaun Mitchell <s.mitchell1998@hotmail.co.uk>
2021-06-29 22:05:55 +0100

    Added a network class to the python module.

--- DeepNetwork/main.py
+++ DeepNetwork/main.py
@@ -1,9 +1,13 @@
-from matrixMultiple import *
+from network import network
+import numpy as np
 
-Alayer = np.array([1, 1])
-weightMatrix = np.array([[1, 2], [3, 4]])
+myNetwork = network(5)
+myNetwork.add_layer(4)
+myNetwork.add_layer(6)
 
-Blayer = calculate_matrix_multiple(weightMatrix, Alayer)
+result = myNetwork.run()
+
+values = np.ctypeslib.as_array(result.values, shape=(6,))
 
 e = 5
 
--- DeepNetwork/matrixMultiple.py
+++ DeepNetwork/matrixMultiple.py
@@ -1,17 +1,12 @@
 from matrixToArray import *
 
 
-def calculate_matrix_multiple(a, b):
+def calculate_matrix_multiple(A, B):
 
-    a = a.astype(np.float32)
-    b = b.astype(np.float32)
     deep_network = windll.LoadLibrary(r"..\x64\Release\DeepNetwork.Infrastructure.dll")
 
     matrix_multiply = deep_network.matrixMultiply
     matrix_multiply.argtypes = [matrix, matrix, matrix]
-
-    A = getMatrixFromArray(a)
-    B = getMatrixFromArray(b)
 
     c_count = A.rows * B.cols
     c_values = (c_float * c_count)()
@@ -20,4 +15,4 @@ def calculate_matrix_multiple(a, b):
 
     matrix_multiply(A, B, C)
 
-    return np.ctypeslib.as_array(c_values, shape=(c_count,))
+    return C
