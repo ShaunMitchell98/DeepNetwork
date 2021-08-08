2afa650e05e26ce4a0afccd7b20dc3644d148a09
Shaun Mitchell <s.mitchell1998@hotmail.co.uk>
2021-08-08 17:40:54 +0100

    Refactored, PyNetwork code into Infrastructure project, added batch adjustments to weights.

--- DeepNetwork/loss.py
+++
@@ -1,7 +0,0 @@
-def loss(expectedOutput, actualOutput):
-
-    loss = 0
-    for i in range(0, expectedOutput.rows * expectedOutput.cols):
-        loss += (expectedOutput[i] - actualOutput[i]) ** 2
-
-    return loss
--- DeepNetwork/matrix.py
+++
@@ -1,7 +0,0 @@
-from ctypes import *
-
-class matrix(Structure):
-    _fields_ = [("values", POINTER(c_double)),
-                ("rows", c_int),
-                ("cols", c_int)]
-
