59964446199e515f5eaeca5975f77a014f9d4fc3
Shaun Mitchell <s.mitchell1998@hotmail.co.uk>
2022-04-17 21:12:27 +0100

    Fixed issues where C API functions weren't exported.

--- PyNet/PyNet/PyNet/VariableLearningSettings.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-
-
-class VariableLearningSettings(ctypes.Structure):
-    _fields_ = [
-        ("ErrorThreshold", ctypes.c_double),
-        ("LRDecrease", ctypes.c_double),
-        ("LRIncrease", ctypes.c_double)
-    ]
